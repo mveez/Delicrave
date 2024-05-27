2d60231d2f6b52953fc40bf8779e7bf89586bbe9
mveez <marievurb@gmail.com>
2024-05-27 20:33:51 -0300

    correccion 2

---
+++ src/components/navbar.js
@@ -0,0 +1,30 @@
+let state = true;
+
+function navbarClick() {
+
+    const navBarBottom = document.getElementById("navBarBottom");
+    const firstSection = document.getElementsByClassName("firstSection");
+
+    if(state == true) {
+        navBarBottom.style.display = "flex";
+        firstSection[0].style.margin = "38rem 0rem";
+        state = false;
+    } else {
+        navBarBottom.style.display = "none";
+        firstSection[0].style.margin = "12rem 0rem";
+        state = true;
+    }
+}
+
+function navBbar() {
+  document.getElementById("navBar").innerHTML = <><div class="navBarTop">
+    <a href="/src/index.html">DELICRAVE</a>
+    <p onclick="navbarClick()">☰</p>
+  </div><ul id="navBarBottom">
+      <li><a href="/src/index.html">INICIO</a></li>
+      <li><a href="/src/views/catalogue/catalogue.html">CATALOGO</a></li>
+      <li><a href="/src/index.html#aboutUs">SOBRE NOSOTROS</a></li>
+      <li><a href="/src/views/contactUs/contactUs.html">CONTACTO</a></li>
+      <li><a href="/src/views/locations/locations.html">LOCACION</a></li>
+    </ul></>;
+}
--- src/scripts/index (2).js
+++ src/scripts/index (2).js
@@ -37,20 +37,3 @@ function navBbar() {
                                                   '<li><a href="/src/views/locations/locations.html">LOCACION</a></li>'+
                                                 '</ul>';
 }
-let state = true;
-
-function navbarClick() {
-
-    const navBarBottom = document.getElementById("navBarBottom");
-    const firstSection = document.getElementsByClassName("firstSection");
-
-    if(state == true) {
-        navBarBottom.style.display = "flex";
-        firstSection[0].style.margin = "38rem 0rem";
-        state = false;
-    } else {
-        navBarBottom.style.display = "none";
-        firstSection[0].style.margin = "12rem 0rem";
-        state = true;
-    }
-}
